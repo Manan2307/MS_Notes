7212cfcc1c1fe936ca3c117b8312dea9a59ff815
Manan Shah <mananshah2307@gmail.com>
2021-05-20 12:22:40 +0530

    MS Notes

---
+++ android/app/src/main/java/com/example/ms_notepad/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ms_notepad;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/note.dart
@@ -0,0 +1,50 @@
+class Note{
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, [this._description]);
+
+  Note.withID(this._id, this._title, this._date, this._priority, [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get priority => _priority;
+
+  set title( String newTitle) => this._title = newTitle;
+
+  set description( String newDescription) => this._description = newDescription;
+
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date( String newDate) => this._date= newDate;
+
+  Map<String, dynamic> toMap(){
+    var map = Map<String, dynamic>();
+    if (id != null){
+      map['id']= _id;
+    }
+    map['description']= _description;
+    map['title']= _title;
+    map['priority']= _priority;
+    map['date']= _date;
+
+    return map;
+  }
+
+  Note.fromMapObject(Map<String, dynamic> map){
+    this._id= map['id'];
+    this._title= map['title'];
+    this._description= map['description'];
+    this._priority= map['priority'];
+    this._date= map['date'];
+  }
+}
